f0d6b2466772324894cbb9e8fcf68e9ed3ff060c
Benjameslane <ben.james.lane9>
2023-08-14 13:34:51 -0600

    added functionality to graph and the ability to record weights and dates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries'
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
 
 function App() {
 
-const [entries, setEntries] = useState([{weight:175,date: '11-23-2021'}])
+const [entries, setEntries] = useState([{weight:175,date: '8-14-2023'}])
 
 function addNewEntry(entry){
   let tempEntries = [...entries, entry];
@@ -14,6 +15,7 @@ function addNewEntry(entry){
     <div>
         <DisplayEntries parentEntries = {entries}/>
         <AddEntryForm addNewEntryProperty={addNewEntry}/>
+        <EntriesChartTracker parentEntries = {entries}/>
     </div>
   );
 }
